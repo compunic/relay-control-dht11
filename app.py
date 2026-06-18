fc331d26dc90968c7b14b4161f31b55e4de4215b
compunic <56836891+compunic@users.noreply.github.com>
2026-06-18 08:02:02 +0700

    Refactor app.py and change database name

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-DB = "relaydht.db"
+DB = "suhudankelembban.db"
 
 # ==================================================
 # DATABASE
